b666364e778069714676ed755e1b1b3155325f99
有吉　貴紀 <>
2021-04-27 17:47:13 +0900

    requested_shiftsで使用するcomponentsを作成

---
+++ app/javascript/RequestedShiftDetail.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <table>
+      <tbody>
+        <tr>
+          <th>出勤者名</th>
+          <th>年齢</th>
+          <th>勤務区分</th>
+        </tr>
+        <tr v-for="e in users" :key="e.id">
+          <td>{{ e.name }}</td>
+          <td>{{ e.age }}</td>
+          <td>{{ e.work_status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      users: [],
+    }
+  },
+  mounted () {
+    axios
+      .get('/api/v1/users.json')
+      .then(response => (this.users = response.data))
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
