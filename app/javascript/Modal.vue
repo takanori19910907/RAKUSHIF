b19bf29035add752d2c32259766351051f596ed8
有吉　貴紀 <>
2021-04-30 16:00:48 +0900

    modal内にformを実装

--- app/javascript/Modal.vue
+++ app/javascript/Modal.vue
@@ -3,6 +3,7 @@
   <div id="overlay">
     <div id="modal">
       <p>{{ year }}年{{ month }}月{{ day }}日の出勤希望を入力</p>
+      <form @submit.prevent="addShift">
       <select v-model="selected1">
         <option disabled value="">出勤希望時間</option>
         <option v-for="clockIn in clockIns" v-bind:val="clockIn.time" v-bind:key="clockIn.id">
@@ -18,7 +19,8 @@
         <p>出勤:{{ selected1 }}</p>
         <p>退勤:{{ selected2 }}</p>
         <p>上記の時間帯でシフト希望を提出します</p>
-        <button >提出</button>
+        <button  type="submit" >提出</button>
+      </form>
     <button @click="$emit('close')">閉じる</button>
     </div>
   </div>
@@ -47,6 +49,17 @@ export default {
     'month',
     'day'
   ],
+  
+  methods: {
+    addShift: function() {
+      this.$store.dispatch('addShift', {
+        clockIn: this.selected1,
+        clockOut: this.selected2
+        })
+      this.selected1= ""
+      this.selected2= ""
+    },
+  }
   
 }
 </script>
