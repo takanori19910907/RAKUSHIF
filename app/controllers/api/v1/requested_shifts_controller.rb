a90e2830589caf11384728a8ddafa1e753996e0e
有吉　貴紀 <>
2021-05-02 17:50:08 +0900

    希望シフト作成の処理をrequested_shifts_controllerに追加

--- app/controllers/api/v1/requested_shifts_controller.rb
+++ app/controllers/api/v1/requested_shifts_controller.rb
@@ -6,6 +6,8 @@ class Api::V1::RequestedShiftsController < ApplicationController
   def index
     shifts = RequestedShift.all
     render json: shifts
+    current_user = current_user
+    render json: current_users
   end
 
   def show
@@ -13,5 +15,23 @@ class Api::V1::RequestedShiftsController < ApplicationController
     render json: shifts
   end
 
+  def create
+    @records = []
+    params.require(:shifts).each do |record|
+      clockIn = "2021-5-2-" + record[:clockIn] + ":00"
+      clockOut = "2021-5-2-" + record[:clockOut] + ":00"
+      shift = current_user.requested_shifts.build(
+        shop_id: current_user.shop_id,
+        clock_in: clockIn,
+        clock_out: clockOut,
+      )
+      shift.save
+    end
+    redirect_to root_path
+  end
+
   private
+    def records_permit(params)
+      params.permit(:clockIn, :clockOut)
+    end
 end
