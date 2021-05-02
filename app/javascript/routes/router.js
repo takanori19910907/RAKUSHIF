72aad96957ab29ac6847f22692d1f14d1e9bcf7e
有吉　貴紀 <>
2021-05-02 17:50:45 +0900

    不要なコードの削除

--- app/javascript/routes/router.js
+++ app/javascript/routes/router.js
@@ -3,31 +3,14 @@ import Router from "vue-router";
 Vue.use(Router);
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
-// import RequestedShift from '../components/RequestedShift.vue';
-// import RequestedShiftIndex from 'RequestedShiftIndex.vue';
-// import RequestedShiftDetail from 'RequestedShiftDetail.vue';
 
 const router = new Router({
   mode: "history",
   routes: [
-     //ルーティングの設定
-    // { path: '/',
-    //   component: RequestedShift,
-    // },
-    // { path: '/2021.04.25',
-    //   component: RequestedShiftIndex,
-    // },
-    // { path: '/2021.04.25/:id(\\d+)',
-    //   component: RequestedShiftDetail,
-    // },
     {
       path: "/admin/requested_shifts",
       component: Home,
     },
-    // {
-    //   path: "/admin/requested_shifts/:id",
-    //   component: About,
-    // },
     {
       path: "/admin/requested_shifts/about", 
       component: About
