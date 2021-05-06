bef0479afee799e887d025510e8294e922bdcc4b
有吉　貴紀 <>
2021-05-06 23:57:09 +0900

    axiosのgetメソッドを使い、App.Vueでユーザー本人のデータを取得

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <Header :user="userData"></Header>
       <router-view :shifts="shifts"></router-view>
   </div>
 </template>
-
 <script>
   import { mapState } from 'vuex'
+  import axios from 'axios';
   import Header from "./components/Header.vue";
   export default {
     components: {
@@ -14,7 +14,13 @@
     },
     data: function() {
       return {
+        userData: {}
       };
+    },
+    mounted () {
+      axios
+        .get('/api/v1/requested_shifts.json')
+        .then(response => (this.userData = response.data))
     },
 
     computed: {
