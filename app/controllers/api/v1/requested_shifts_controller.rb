e06add0ab48481fb2a002368230bf94ed43d0db4
有吉　貴紀 <>
2021-05-09 19:47:27 +0900

    destroyアクションを追加

--- app/controllers/api/v1/requested_shifts_controller.rb
+++ app/controllers/api/v1/requested_shifts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RequestedShiftsController < ApplicationController
+  before_action :set_shiftdata, only: [:destroy]
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { error: '404 not found' }, status: 404
   end
@@ -18,7 +19,6 @@ class Api::V1::RequestedShiftsController < ApplicationController
     params.require(:shifts).each do |record|
       clockIn = record[:year].to_s + "-" + record[:month].to_s + "-" + record[:day].to_s + "-" +  record[:clockIn] + ":00"
       clockOut = record[:year].to_s + "-" + record[:month].to_s + "-" + record[:day].to_s + "-" +  record[:clockOut] + ":00"
-      binding.pry
       shift = current_user.requested_shifts.build(
         shop_id: current_user.shop_id,
         clock_in: clockIn,
@@ -28,4 +28,17 @@ class Api::V1::RequestedShiftsController < ApplicationController
     end
     redirect_to root_path
   end
+
+  def destroy
+    @shift.destroy
+    head :no_content
+    # render json: shifts
+    # redirect_to root_path
+    
+  end
+
+  private
+    def set_shiftdata
+      @shift = RequestedShift.find_by(id: params[:requested_shift][:id])
+    end
 end
