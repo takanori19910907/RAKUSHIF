d31ed14ede0a399036b541882826f85ce980672d
有吉　貴紀 <>
2021-05-13 17:12:24 +0900

    ログイン関連のメソッドをhelperからcontrollerに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
   include SessionsHelper
   def login_check
     if current_user.nil?
       redirect_to root_url, danger: "ログインしてください"
     end
+  end
 
   def admin_user
       redirect_to(root_path) unless current_user.admin
-    end
   end
+
+  private
+    #ログイン中かどうか確認し、そうでない場合はログイン処理を行う
+    def current_user
+      if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id])
+      end
+    end
 end
