8cfbb06fc365b4814c7eb0634964d7c1b16fd944
有吉　貴紀 <>
2021-07-14 03:05:35 +0900

    不要なファイルを削除

--- test/controllers/api/v1/admin/users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Api::V1::Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_admin_users_index_url
-    assert_response :success
-  end
-
-end
