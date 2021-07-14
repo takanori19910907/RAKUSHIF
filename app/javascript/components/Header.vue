bb12cb0aae83cb26955a8191063d228acce4f489
有吉　貴紀 <>
2021-07-14 14:33:58 +0900

    デバッグ用のリンクを追加

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <div v-if="hasAdmin">
-      <router-link to="/admin/requested_shifts/">作成シフトの新規作成</router-link>
+    <div v-if="hasAdmin == true">
+      <router-link to="/admin/requested_shifts/">確定シフトの作成</router-link>
       <router-link to="/admin/fixed_shifts/">確定シフト表</router-link>
+
+      <!-- 以下デバッグ用リンク -->
+
+      <span>◀︎  管理者/ユーザー  ▶︎</span>
+      <router-link to="/staff/requested_shifts/">希望シフト提出</router-link>
+      <router-link :to="{ name : 'RequestedShiftsDetail', params : { id: this.userID }}">提出済み希望シフト一覧</router-link>
+            <router-link to="/staff/fixed_shifts/">確定シフト表</router-link>
+
+      <!-- ここまで 開発完了時に削除する -->
+
     </div>
     <div v-else>
       <router-link to="/staff/requested_shifts/">希望シフト表</router-link>
@@ -13,15 +23,11 @@
 </template>
 <script>
   export default {
-    
-    data() {
-      return {
-      };
-    },
 
     props: [
       'userID',
       'hasAdmin'
     ]
   };
+
 </script>
