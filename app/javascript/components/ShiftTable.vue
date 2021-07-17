5830e0f95983afbccaf921fa9e604a484ac634b7
有吉　貴紀 <>
2021-07-17 16:21:34 +0900

    シフトを一覧表示するためのcomponentを作成

---
+++ app/javascript/components/ShiftTable.vue
@@ -0,0 +1,63 @@
+<!-- シフトを一覧表示するテーブル -->
+
+<template>
+<div>
+
+  <div v-if="filteredShiftData.length">
+    <table>
+      <tbody>
+        <tr>
+          <th>氏名</th>
+          <th>年齢</th>
+          <th>勤務ステータス</th>
+          <th>希望出勤時間</th>
+          <th>希望退勤時間</th>
+        </tr>
+        <tr v-for="(shift) in filteredShiftData" :key="shift.id" >
+          <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
+          <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
+          <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
+          <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div v-else>
+    <p>出勤予定の方がいません</p>
+  </div>
+</div>
+</template>
+
+<script>
+  import dayjs from "dayjs"
+  import UserName from "./UserName.vue"
+  import UserAge from "./UserAge.vue"
+  import UserWorkStatus from "./UserWorkStatus.vue"
+  import RequestedClockInTime from "./RequestedClockInTime.vue"
+  import RequestedClockOutTime from "./RequestedClockOutTime.vue"
+
+export default({
+  components: {
+    UserName,
+    UserAge,
+    UserWorkStatus,
+    RequestedClockInTime,
+    RequestedClockOutTime,
+  },
+
+  props: [
+    'fixedShifts',
+    'year',
+    'month',
+    'date'
+  ],
+
+  computed: {
+      // カレンダーで指定した日付のシフト情報とそのシフトのユーザー情報を取得し表示する
+      filteredShiftData() {
+        const calendarDate = dayjs(this.year + "-" + this.month + "-" + this.date).format("DD/MM/YYYY")
+        return this.fixedShifts.filter(shift => calendarDate === dayjs(shift.clock_in).format("DD/MM/YYYY"))
+      },
+    },
+})
+</script>
