f5d693142432b284dcc6fb026edc73c0f2a4b35c
有吉　貴紀 <>
2021-07-17 15:18:11 +0900

    jbuilderを使用し、バックエンド側にシフトデータとユーザーデータの紐付け処理を実装

--- app/controllers/api/v1/staff/fixed_shifts_controller.rb
+++ app/controllers/api/v1/staff/fixed_shifts_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Staff::FixedShiftsController < ApplicationController
   def index
-    shifts = FixedShift.all
-    render json: shifts
+    @shifts = FixedShift.all
+  end
+
+  def current_user_fixed_shifts
+    shifts = current_user.fixed_shifts
+    render :shifts
   end
 end
