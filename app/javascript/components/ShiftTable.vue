ead04d657580298b6c8ddc0570932aa00a24f609
有吉　貴紀 <>
2021-07-17 16:38:11 +0900

    親componentから受け取ったadmin属性で表示を切り替えるよう改修

--- app/javascript/components/ShiftTable.vue
+++ app/javascript/components/ShiftTable.vue
@@ -1,30 +1,49 @@
 <!-- シフトを一覧表示するテーブル -->
 
 <template>
-<div>
+  <div>
+    <div v-if="filteredShiftData.length">
+      
+      <table v-if="this.admin===true">
+        <tbody>
+          <tr>
+            <th>氏名</th>
+            <th>年齢</th>
+            <th>勤務ステータス</th>
+            <th>希望出勤時間</th>
+            <th>希望退勤時間</th>
+          </tr>
+          <tr v-for="(shift) in filteredShiftData" :key="shift.id" >
+            <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
+            <td><userAge :key="item.id" :userAge="item.user.age" ></userAge></td>
+            <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
+            <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
+            <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
+          </tr>
+        </tbody>
+      </table>
 
-  <div v-if="filteredShiftData.length">
-    <table>
-      <tbody>
-        <tr>
-          <th>氏名</th>
-          <th>年齢</th>
-          <th>勤務ステータス</th>
-          <th>希望出勤時間</th>
-          <th>希望退勤時間</th>
-        </tr>
-        <tr v-for="(shift) in filteredShiftData" :key="shift.id" >
-          <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
-          <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
-          <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
-          <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div v-else>
-    <p>出勤予定の方がいません</p>
-  </div>
+      <table v-else>
+        <tbody>
+          <tr>
+            <th>氏名</th>
+            <th>勤務ステータス</th>
+            <th>希望出勤時間</th>
+            <th>希望退勤時間</th>
+          </tr>
+          <tr v-for="(shift) in filteredShiftData" :key="shift.id" >
+            <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
+            <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
+            <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
+            <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+    <div v-else>
+      <p>出勤予定の方がいません</p>
+    </div>
 </div>
 </template>
 
@@ -49,7 +68,8 @@ export default({
     'fixedShifts',
     'year',
     'month',
-    'date'
+    'date',
+    'admin'
   ],
 
   computed: {
