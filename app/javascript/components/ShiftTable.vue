2772f388058b29dec074e6bb6f70e6e0200bbf70
有吉　貴紀 <>
2021-07-19 17:18:03 +0900

    冗長化したコードをリファクタリング

--- app/javascript/components/ShiftTable.vue
+++ app/javascript/components/ShiftTable.vue
@@ -4,7 +4,7 @@
   <div>
     <div v-if="filteredShifts.length">
       
-      <table v-if="admin">
+      <table>
         <tbody>
           <tr>
             <th>氏名</th>
@@ -15,31 +15,13 @@
           </tr>
           <tr v-for="(shift) in filteredShifts" :key="shift.id" >
             <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
-            <td><userAge :key="item.id" :userAge="item.user.age" ></userAge></td>
+            <td v-if="admin"><userAge :key="item.id" :userAge="item.user.age" ></userAge></td>
             <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
             <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
             <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
           </tr>
         </tbody>
       </table>
-
-      <table v-else>
-        <tbody>
-          <tr>
-            <th>氏名</th>
-            <th>勤務ステータス</th>
-            <th>希望出勤時間</th>
-            <th>希望退勤時間</th>
-          </tr>
-          <tr v-for="(shift) in filteredShifts" :key="shift.id" >
-            <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
-            <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
-            <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
-            <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
-          </tr>
-        </tbody>
-      </table>
-
     </div>
     <div v-else>
       <p>出勤予定の方がいません</p>
