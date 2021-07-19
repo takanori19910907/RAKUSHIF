593cd150b0977305821040248fc65e479253cb0c
有吉　貴紀 <>
2021-07-19 16:53:26 +0900

    シフトデータ取得でデフォルト設定とする日付範囲を変更

--- app/controllers/api/v1/staff/fixed_shifts_controller.rb
+++ app/controllers/api/v1/staff/fixed_shifts_controller.rb
@@ -4,19 +4,19 @@ class Api::V1::Staff::FixedShiftsController < ApplicationController
   def index
     # 所属店舗のシフトデータを検索し、現在から１ヶ月先の期間で絞り込んで取得
     @shifts = FixedShift.where(shop_id: current_user.shop_id, clock_in: @from...@to)
+    binding.pry
   end
 
   def current_user_fixed_shifts
-    binding.pry
     @shifts = current_user.fixed_shifts.where(clock_in: @from...@to)
     render :shifts
   end
 
   private
     def get_date_for_twomonths
-      #取得するシフトの絞り込みのため、デフォルトの設定期間として２ヶ月間を定義
+      #取得するシフトの絞り込みのため、デフォルトの設定期間として日付範囲を定義(現在の日にちを基準に前後１ヶ月)
       today = Time.current.at_beginning_of_day
       @from = (today - 1 .month)
-      @to = (@from + 1.month)
+      @to = (today + 1.month)
     end
 end
