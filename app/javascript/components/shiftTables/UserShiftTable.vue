b2c041b65c0d136eab26ec192c29cddd2dd66bfa
有吉　貴紀 <>
2021-07-23 13:48:08 +0900

    リファクタリングに伴い修正

--- app/javascript/components/shiftTables/UserShiftTable.vue
+++ app/javascript/components/shiftTables/UserShiftTable.vue
@@ -8,20 +8,18 @@
         <tbody>
           <tr>
             <th>氏名</th>
-            <th>年齢</th>
             <th>勤務ステータス</th>
             <th>希望出勤時間</th>
             <th>希望退勤時間</th>
           </tr>
-          <tr v-for="(shift, index) in shifts" :key="shift.id" >
+          <tr v-for="(shift) in shifts" :key="shift.id" >
             <td><userName :key="shift.id" :userName="shift.user.name" ></userName></td>
-            <td><userAge :key="shift.id" :userAge="shift.user.age" ></userAge></td>
             <td><userWorkStatus :key="shift.id" :userData="shift.user" ></userWorkStatus></td>
             <td><requestedClockInTime :key="shift.id" :clockIn="shift.clock_in" ></requestedClockInTime></td>
             <td><requestedClockOutTime :key="shift.id" :clockOut="shift.clock_out" ></requestedClockOutTime></td>
             <div v-if="editPermission">
-              <button @click="$emit('openModal', shift, index)">修正</button>
-              <button @click="$emit('sendDeleteShiftId', shift.id, index)">削除</button>
+              <button @click="$emit('editButtonPushed', shift)">修正</button>
+              <button @click="$emit('deleteButtonPushed', shift.id)">削除</button>
             <div>
           </tr>
         </tbody>
@@ -35,7 +33,6 @@
 
 <script>
   import UserName from "./modules/UserName.vue"
-  import UserAge from "./modules/UserAge.vue"
   import UserWorkStatus from "./modules/UserWorkStatus.vue"
   import RequestedClockInTime from "./modules/RequestedClockInTime.vue"
   import RequestedClockOutTime from "./modules/RequestedClockOutTime.vue"
@@ -43,7 +40,6 @@
 export default({
   components: {
     UserName,
-    UserAge,
     UserWorkStatus,
     RequestedClockInTime,
     RequestedClockOutTime,
