1002415b34550a0dfc641f3872012ac6e5009eef
有吉　貴紀 <>
2021-07-30 17:55:54 +0900

    ルーティングをhistoryモードに変更

--- app/javascript/routes/router.js
+++ app/javascript/routes/router.js
@@ -7,32 +7,33 @@ import FixedShifts from "../components/organisms/staff/FixedShifts.vue";
 import RequestedShiftsIndex from "../components/organisms/admin/RequestedShiftsIndex.vue";
 import FixedShiftsIndex from "../components/organisms/admin/FixedShiftsIndex.vue";
 const router = new Router({
+  mode: "history",
   routes: [
     {
-      path: "/staff/requested_shifts/",
+      path: "/users/top/staff/requested_shifts/",
       component: RequestedShifts,
     },
 
     {
-    path: "/staff/requested_shifts/id", 
+    path: "/users/top/staff/requested_shifts/id", 
       name: "RequestedShiftsDetail",
       component: RequestedShiftsDetail
     },
 
     {
-      path: "/staff/fixed_shifts/",
+      path: "/users/top/staff/fixed_shifts/",
       name: "FixedShifts",
       component: FixedShifts,
     },
 
     {
-      path: "/admin/requested_shifts/",
+      path: "/users/top/admin/requested_shifts/",
       name: "RequestedShiftsIndex",
       component: RequestedShiftsIndex,
     },
 
     {
-      path: "/admin/fixed_shifts/",
+      path: "/users/top/admin/fixed_shifts/",
       name: "FixedShiftsIndex",
       component: FixedShiftsIndex,
     },
